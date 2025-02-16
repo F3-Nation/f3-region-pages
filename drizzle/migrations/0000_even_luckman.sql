f46c88ede9f8444dfcbd97d4fc23b6073b70973b
Patrick Taylor <1963845+pstaylor-patrick@users.noreply.github.com>
2025-02-16 08:38:45 -0600

    db:generate:migration points

---
+++ drizzle/migrations/0000_even_luckman.sql
@@ -0,0 +1,19 @@
+CREATE TABLE "points" (
+	"Group" varchar NOT NULL,
+	"Time" varchar NOT NULL,
+	"Type" varchar NOT NULL,
+	"Region" varchar NOT NULL,
+	"Website" varchar NOT NULL,
+	"Notes" varchar NOT NULL,
+	"Marker Icon" varchar NOT NULL,
+	"Marker Color" varchar NOT NULL,
+	"Icon Color" varchar NOT NULL,
+	"Custom Size" varchar NOT NULL,
+	"Name" varchar NOT NULL,
+	"Image" varchar NOT NULL,
+	"Description" varchar NOT NULL,
+	"Location" varchar NOT NULL,
+	"Latitude" varchar NOT NULL,
+	"Longitude" varchar NOT NULL,
+	"Entry ID" varchar PRIMARY KEY NOT NULL
+);
